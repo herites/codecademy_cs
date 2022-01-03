1a2c1b93dbfed6c158a4ed6b967d4bd9da74beb3
herites <95563388+herites@users.noreply.github.com>
2022-01-03 11:59:42 +0100

    refactor: change code to match PEP9 guidelines

--- cs_101/tic_tac_toe.py
+++ cs_101/tic_tac_toe.py
@@ -2,7 +2,7 @@ from random import randint
 
 
 class Player:
-    def __init__(self, marker="X", name=input("What is your name? ")) -> None:
+    def __init__(self, name=None, marker="X"):
         self.name = name
         self.marker = marker
 
@@ -25,16 +25,16 @@ class Player:
 
     def play_turn(self, board):
         print(f"This is turn {board.turn}, {self.name} acts.")
-        board.turn += 1
+        board.increment_turn()
         self.get_marker_loc(board)
         self.place_marker(board)
         board.check_win(self.marker)
-        if board.won == True:
+        if board.is_won:
             print(f"{self.name} wins!")
 
 
 class Computer(Player):
-    def __init__(self, name="Computer", marker="O") -> None:
+    def __init__(self, name="Computer", marker="O"):
         super().__init__()
         self.name = name
         self.marker = marker
@@ -48,15 +48,17 @@ class Computer(Player):
 
 
 class Board:
-    turn = 1
-    won = False
-
-    def __init__(self) -> None:
+    def __init__(self):
         self.play_area = [[" " for col in range(0, 3)] for row in range(0, 3)]
+        self.turn = 1
+        self.is_won = False
 
     def show(board):
         for i in board.play_area:
             print(i, sep="\n")
+
+    def increment_turn(self):
+        self.turn += 1
 
     def update(self, row, column, marker):
         self.play_area[row - 1][column - 1] = marker
@@ -73,7 +75,7 @@ class Board:
     def row_win(self, marker):
         for row in self.play_area:
             if row.count(marker) == 3:
-                self.won = True
+                self.is_won = True
 
     def column_win(self, marker):
         cols = []
@@ -84,12 +86,12 @@ class Board:
                 col_index += 1
         for i in cols:
             if i.count(marker) == 3:
-                self.won = True
+                self.is_won = True
 
     def left_to_right_diagonal_win(self, marker):
         diagonal = [row[index] for index, row in enumerate(self.play_area)]
         if diagonal.count(marker) == 3:
-            self.won = True
+            self.is_won = True
 
     def right_to_left_diagonal_win(self, marker):
         col_index = len(self.play_area[-1]) - 1
@@ -99,37 +101,38 @@ class Board:
             if col_index > 0:
                 col_index -= 1
         if diagonal.count(marker) == 3:
-            self.won = True
+            self.is_won = True
 
 
 def main():
-    board, player1, computer = game_setup()
+    board = Board()
+    name = input("What is your name? ")
+    player1 = Player(name)
+    computer = Computer()
+    print_welcome(player1.name)
+    board.show()
     while board.turn <= 9:
         player1.play_turn(board)
-        if board.won == True:
+        if board.is_won:
             break
         computer.play_turn(board)
-        if board.won == True:
+        if board.is_won:
             break
         if board.turn == 9:
             print("Board is full, nobody wins.")
+            break
 
 
-def game_setup():
-    board = Board()
-    player1 = Player()
-    computer = Computer()
+def print_welcome(player):
     print(
         f"""
-Welcome to Tic-Tac-Toe {player1.name}!
+Welcome to Tic-Tac-Toe {player}!
 You will play against a very stupid computer, try not to lose!
 Error checking is currently not implemented for user input, do not go out of bounds.
 Valid marker positions are integers between 1 and 3.
-You will be {player1.marker} and you will go first.
+You will be {player} and you will go first.
 This is the board you will be playing on:"""
     )
-    board.show()
-    return board, player1, computer
 
 
 if __name__ == "__main__":
